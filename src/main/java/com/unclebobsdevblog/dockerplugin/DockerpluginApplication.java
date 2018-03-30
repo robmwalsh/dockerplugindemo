ef3a52d2c33a97f3de71885086dfb515ec022351
Rob Walsh <robert.walsh@contextpc.com>
2018-03-30 16:52:44 +1100

    initial commit

---
+++ src/main/java/com/unclebobsdevblog/dockerplugin/DockerpluginApplication.java
@@ -0,0 +1,12 @@
+package com.unclebobsdevblog.dockerplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerpluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerpluginApplication.class, args);
+	}
+}
---
+++ src/test/java/com/unclebobsdevblog/dockerplugin/DockerpluginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.unclebobsdevblog.dockerplugin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerpluginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
